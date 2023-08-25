54dd8cb4ea029bfd6ca8765dd8613f4966b59e0e
Sarath Sekar <119506630+SARATH-AERO@users.noreply.github.com>
2023-08-25 14:54:00 +0530

    Update Program.cs
    added some int var at last

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,6 +15,9 @@ namespace ConsoleApp1
             string newio = "you are in master branch";
             string mes = "2nd commit is made";
             string hithere = "you are in master branch";
+            int vdal = 56556;
+            int koo = 56565;
+            int dfsdl = 342409;
         }
     }
 }
