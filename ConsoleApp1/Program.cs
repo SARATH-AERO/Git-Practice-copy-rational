514bb4339f4d50a5a1d94a0c5da67e01af19407c
Sarath Sekar <saraths@keysoftwareinc.com>
2023-08-25 12:50:40 +0530

    revert back to 2nd commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,12 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             string hi = "both master & a branch rebased";
             int value = 567;
             int brancha = 4566;
             int branchnew = 453;
             string newio = "you are in master branch";
             string mes = "2nd commit is made";
+=======
+<<<<<<< HEAD
+=======
+            string hi = "you are in master branch";
+>>>>>>> parent of f986133 (2nd commit for master)
+>>>>>>> d1c84d1 (revert back to 2nd commit)
         }
     }
 }
