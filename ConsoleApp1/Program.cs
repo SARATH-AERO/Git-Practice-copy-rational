9702c39b94b81d11c953236468718c30e75ec93e
Sarath Sekar <saraths@keysoftwareinc.com>
2023-08-25 14:46:32 +0530

    last commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,19 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             string hi = "both master & a branch rebased";
             int value = 567;
             int brancha = 4566;
             int branchnew = 453;
             string newio = "you are in master branch";
             string mes = "2nd commit is made";
-=======
-<<<<<<< HEAD
-=======
-            string hi = "you are in master branch";
->>>>>>> parent of f986133 (2nd commit for master)
->>>>>>> d1c84d1 (revert back to 2nd commit)
+            string hithere = "you are in master branch";
         }
     }
 }
